0febe37c40de4a7de82d7643b2537206f48f1b3b
Jan von Rickenbach <janvonrickenbach@MBP-de-Jehanne.localdomain>
2019-07-26 14:13:02 +0200

    working version with calibration mode

--- client/index.js
+++ client/index.js
@@ -1,49 +1,113 @@
 // let poseNet
-let video
-let classifier
+let video;
+let classifier;
+let classificationOutput;
+
+let size = 200;
+
+let outputWindow = {
+    x: 0,
+    y: 0,
+    sizeX: size,
+    sizeY: size
+};
+
+let captureWindow = {
+    x: outputWindow.sizeX,
+    y: 0,
+    offsetX: 400,
+    offsetY:0,
+    sizeX: 200,
+    sizeY: 200
+};
+
+let previewWindow = {
+    x: outputWindow.sizeX+captureWindow.sizeX,
+    y: 0,
+    offsetX: captureWindow.offsetX,
+    offsetY: captureWindow.offsetY,
+    sizeX: captureWindow.sizeX,
+    sizeY: captureWindow.sizeY
+};
+
+
+calibrationMode = true;
+
+function mouseClicked() {
+    calibrationMode = !calibrationMode;
+    clear();
+    video = createCapture(VIDEO);
+}
+
 
 function setup() {
+    video = createCapture(VIDEO);
 
-  createCanvas(1200, 800)
-  video = createCapture(VIDEO)
-  window.setInterval( () => {classify(video).then(d => {
-    console.log(d.label)
-    searchUnsplash(d.label)
-  })}, 5000)
-  // classifier = ml5.imageClassifier('MobileNet', video, onModelReady)
-  // poseNet = ml5.poseNet(video, onModelReady)
+    video.hide();
+    createCanvas(5 * size,1*size);
+
+    window.setInterval(() => {
+        if(!calibrationMode){
+            classify()
+                .then(d => {
+                    classificationOutput && classificationOutput.remove();
+                    classificationOutput = createP(d.label);
+                    return searchUnsplash(d.label)
+                });
+        }
+
+    }, 5000)
 }
 
 function searchUnsplash(keyword) {
-  fetch(`https://cors.ft0.ch/https://unsplash.com/search/photos/${keyword}`)
-    .then(r => r.text())
-    .then(d => {
-      const r = JSON.parse(d.match(/INITIAL_STATE__ = ([^;]*)/)[1])
-      const photoIds = (Object.keys(r.entities.photos))
-      const photo = r.entities.photos[photoIds[Math.floor(Math.random() * photoIds.length)]]
-      loadImage(photo.urls.small, img => {
-        image(img, 0, 0);
-      })
-    })
+    return fetch(`https://cors.ft0.ch/https://unsplash.com/search/photos/${keyword}`)
+        .then(r => r.text())
+        .then(d => {
+            const r = JSON.parse(d.match(/INITIAL_STATE__ = ([^;]*)/)[1])
+            const photoIds = (Object.keys(r.entities.photos))
+            const photo = r.entities.photos[photoIds[Math.floor(Math.random() * photoIds.length)]]
+            loadImage(photo.urls.small, img => {
+                image(img, outputWindow.x, outputWindow.y, outputWindow.sizeX, outputWindow.sizeY);
+            })
+        })
+}
+
+function draw() {
+    if(calibrationMode){
+        image(video,previewWindow.x, previewWindow.y, previewWindow.sizeX, previewWindow.sizeY,previewWindow.offsetX,previewWindow.offsetY,previewWindow.sizeX,previewWindow.sizeY)
+    }
 }
 
 
+function classify() {
+    video = createCapture(VIDEO);
 
-function classify(video){
-    return getClassifier(video)
-        .then(classifyAndGetFirstResult);
-}
-
-
-function getClassifier(video){
     return new Promise((resolve,reject) => {
-        classifier = ml5.imageClassifier('MobileNet',video);
-        resolve(classifier);
+        setTimeout(() => {
+            if (video) {
+                video.hide();
+                stillImage = video.get(captureWindow.offsetX, captureWindow.offsetY, captureWindow.sizeX, captureWindow.sizeY);
+                image(stillImage, captureWindow.x, captureWindow.y, captureWindow.sizeX, captureWindow.sizeY);
+                return getClassifier(stillImage)
+                    .then(classifier => resolve(classifyAndGetFirstResult(classifier,stillImage)));
+            }
+        }, 500)
+
+    })
+
+}
+
+
+function getClassifier() {
+    return new Promise((resolve, reject) => {
+        classifier = ml5.imageClassifier('MobileNet', () => {
+            resolve(classifier);
+        });
     })
 }
 
-function classifyAndGetFirstResult(classifier) {
-    return classifier.classify()
+function classifyAndGetFirstResult(classifier, stillImage) {
+    return classifier.classify(stillImage)
         .then(results => {
             return {
                 label: results[0].label,
